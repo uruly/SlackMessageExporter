04d81108a0aa1cee495b80c2687ba5a5b5125217
uruly <rk.uru007@gmail.com>
2025-01-24 11:16:03 +0900

    :pick: Fix codeblock parse #2

--- src/exportHTML.ts
+++ src/exportHTML.ts
@@ -14,9 +14,9 @@ export async function saveMessagesToHTML(
     messages.map((message) => {
       const timestamp = formatTimestampToJST(message.timestamp);
       const text = replaceShortcodesWithUnicode(message.text || ""); // ショートコードをUnicodeに変換";
-      const markdownContent = render(text);
+      const markdownContent = render(addNewlinesToCodeBlocks(text));
       const attachments = message.attachments;
-      const userName = message.user?.realName ?? "Unknowon User";
+      const userName = message.user?.realName ?? "Unknown User";
 
       return `<tr>
           <td>${timestamp}</td>
@@ -62,4 +62,17 @@ export async function saveMessagesToHTML(
 
   await Deno.writeTextFile(filePath, htmlContent);
   console.log(`Messages successfully saved to ${filePath}`);
+}
+
+/**
+ * コードブロックの開始部分と終了部分に改行を追加する関数
+ * @param markdown 修正対象の Markdown テキスト
+ * @returns 修正後の Markdown テキスト
+ */
+function addNewlinesToCodeBlocks(markdown: string): string {
+  // 開始部分（```）の後に改行を追加
+  markdown = markdown.replace(/```(\S*?)\r?\n?/g, (_, lang) => `\`\`\`${lang}\n`);
+  // 終了部分（}```）の前に改行を追加
+  markdown = markdown.replace(/\s*```/g, "\n```");
+  return markdown;
 }
